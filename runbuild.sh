cb3993d1d9c8b8a45e7ce8a073301781b134be8d
Prakash71322 <srikrishnaprakashs@hexaware.com>
2023-02-08 12:28:03 +0530

    Image name of backend and frontend changed

--- runbuild.sh
+++ runbuild.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-#mvn -f ecomm-backend/pom.xml clean package -DskipTests
-#docker image build -f Dockerfile.ecomapi -t  hexadevops/ecomapi:latest .
-docker image build -f Dockerfile.ecomui -t hexadevops/ecomui:latest .
-#docker push hexadevops/ecomapi:latest
-docker push hexadevops/ecomui:latest
+mvn -f ecomm-backend/pom.xml clean package -DskipTests
+docker image build -f Dockerfile.ecomapi -t  hexadevops/ecombackend:latest .
+docker image build -f Dockerfile.ecomui -t hexadevops/ecomfrontend:latest .
+docker push hexadevops/ecombackend:latest
+docker push hexadevops/ecomfrontend:latest
 
 #kubectl exec -it postgres-deployment-6d46c57765-lqhpc -n saecom -- bash
 #psql -Utensai --password ecommerce
